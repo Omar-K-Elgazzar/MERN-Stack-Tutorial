923bfa602e69124b9c0fbf8bd67184f65da30869
Omar-Elgazzar <omar.k.elgazzar@gmail.com>
2024-12-24 07:38:02 +0400

    Express App Completed

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,21 @@
+
+require ('dotenv').config()
 const express = require ('express')
 
 //express app
 const app = express()
 
-//listen for requests
-app.listen(4000, () => {
-    console.log("Listening on port 4000")
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
 })
+
+//routes
+app.get ('/', (req, res) => {
+    res.json({mssg:"Welcome to the app"})
+})
+//listen for requests
+app.listen(process.env.PORT, () => {
+    console.log("Listening on port", process.env.PORT)
+})
+
